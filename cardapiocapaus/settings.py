44701d596dbb4a86317fc8dd6b5851bbc1ed7c96
Fabio Rolim <fabiorolim@ifpi.edu.br>
2017-05-31 12:01:16 -0300

    Altera a versão do python para 3.6.1

--- cardapiocapaus/settings.py
+++ cardapiocapaus/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-LOGIN_URL='/login/'
+LOGIN_URL='/usuario/login/'
 LOGOUT_REDIRECT_URL='/usuario/login/'
 LOGIN_REDIRECT_URL='/cardapios/'
 
